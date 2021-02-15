05b4fcb630f0dab3d42528f0403c845acfca7d9f
Welington Silva <welington.manoels@gmail.com>
2021-02-15 14:33:29 -0300

    feat: add library exports

---
+++ src/index.js
@@ -0,0 +1,8 @@
+export { default } from "./logical";
+export { default as and } from "./and";
+export { default as nand } from "./nand";
+export { default as nor } from "./nor";
+export { default as not } from "./not";
+export { default as or } from "./or";
+export { default as xnor } from "./xnor";
+export { default as xor } from "./xor";

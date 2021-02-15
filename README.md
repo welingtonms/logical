291a0ebe50f3c33cea8b585a777525b0e03403c7
Welington Silva <welington.manoels@gmail.com>
2021-02-15 14:31:26 -0300

    chore: add basic infrastructure

---
+++ README.md
@@ -0,0 +1,21 @@
+# @cheesebit/logical
+
+I have always been fascinated by the simplicity of logical operations and how complex logical structures you can build with it.
+
+That's why I wrote this library, to provides a toolset to perform logical operations.
+
+It follows Javascript's default [_truthy_](https://developer.mozilla.org/en-US/docs/Glossary/Truthy) and [_falsy](https://developer.mozilla.org/en-US/docs/Glossary/Falsy)_ definitions.
+
+
+## Instalation
+
+You can add this hook as a dependency by running `npm install @cheesebit/logical` or `yarn add @cheesebit/logical`.
+
+## Supported operations
+
+## Usage guide
+
+```js
+  import logical, { and, or, xor, not, nand, nor, xnor } from '@cheesebit/logical';
+
+```
---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+import { terser } from 'rollup-plugin-terser';
+import analyze from 'rollup-plugin-analyzer';
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import del from 'rollup-plugin-delete';
+import pkg from './package.json';
+import resolve from '@rollup/plugin-node-resolve';
+
+module.exports = [
+  {
+    input: 'src/index.js',
+    output: [
+      {
+        name: 'logical',
+        file: pkg.browser,
+        format: 'umd',
+      },
+      { name: 'logical', file: pkg.main, format: 'cjs' },
+      { name: 'logical', file: pkg.module, format: 'es' },
+    ],
+    plugins: [
+      del({ targets: [`dist/`] }),
+      babel({
+        exclude: 'node_modules/**', // only transpile our source code
+      }),
+      resolve(), // so Rollup can find `ms`
+      commonjs(), // so Rollup can convert `ms` to an ES module
+      terser(),
+      analyze({
+        hideDeps: true,
+        summaryOnly: true,
+        filter: module => /^\/src/.test(module.id),
+      }),
+    ],
+  },
+];

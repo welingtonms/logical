a51135ec7b9e278b980f327c86107dcc158d19ad
Welington Silva <welington.manoels@gmail.com>
2021-02-28 17:26:01 -0300

    chore: add changelog to semantic-release git [skip ci]

--- release.config.js
+++ release.config.js
@@ -37,7 +37,7 @@ module.exports = {
     [
       '@semantic-release/git',
       {
-        assets: ['dist/**/*.{js,map}', 'package.json'],
+        assets: ['dist/**/*.{js,map}', 'package.json', 'CHANGELOG.md'],
         message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
     ],

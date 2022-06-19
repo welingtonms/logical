f42f80570d37a5e7eaeb3d983be6afcbfc7c973e
Welington Silva <welington.manoels@gmail.com>
2022-06-19 19:32:28 -0300

    fix: update config; transfer to welingtonms

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,30 @@
 import { terser } from 'rollup-plugin-terser';
 import analyze from 'rollup-plugin-analyzer';
 import babel from '@rollup/plugin-babel';
-import commonjs from '@rollup/plugin-commonjs';
+
 import del from 'rollup-plugin-delete';
-import pkg from './package.json';
 import resolve from '@rollup/plugin-node-resolve';
 
-module.exports = [
-  {
-    input: 'src/index.js',
-    output: [
-      { name: 'logical', file: pkg.module, format: 'es' },
-    ],
-    plugins: [
-      del({ targets: [`dist/`] }),
-      babel({
-        exclude: 'node_modules/**', // only transpile our source code
-      }),
-      resolve(), // so Rollup can find `ms`
-      commonjs(), // so Rollup can convert `ms` to an ES module
-      terser(),
-      analyze({
-        hideDeps: true,
-        summaryOnly: true,
-        filter: module => /^\/src/.test(module.id),
-      }),
-    ],
-  },
-];
+module.exports = {
+	input: 'src/index.js',
+	output: [
+		{
+			dir: 'dist',
+			format: 'esm',
+			sourcemap: true,
+		},
+	],
+	plugins: [
+		del( { targets: [ `dist/` ] } ),
+		babel( {
+			exclude: 'node_modules/**', // only transpile our source code
+		} ),
+		resolve(), // so Rollup can find `ms`
+		terser(),
+		analyze( {
+			hideDeps: true,
+			summaryOnly: true,
+			filter: ( module ) => /^\/src/.test( module.id ),
+		} ),
+	],
+};
